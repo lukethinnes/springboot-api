084c346fb538420329ed42f659e5d77417d201d2
Luke Thinnes <lukethinnes@yahoo.com>
2021-10-30 20:45:28 -0600

    Added StudentService class.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,26 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.time.LocalDate;
-import java.time.Month;
 
 @RequestMapping(path = "api/v1/student")
 @RestController
 
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                    new Student(
-                        1L,
-                        "Mariam",
-                        "magiam.jama@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                    )
-                ); 
+        return studentService.getStudents();        
     }
-     
 }
 
