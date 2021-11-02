b175a9d73f35fa385ecad2dd096c92da4bd1d14a
Luke Thinnes <lukethinnes@yahoo.com>
2021-11-02 15:36:52 -0600

    Used a transient to remove hard coded age from Student table.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,12 +1,8 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import java.time.Period;
 
 @Entity
 @Table
@@ -26,6 +22,7 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student() {
@@ -35,13 +32,12 @@ public class Student {
     public Student(Long id, 
                     String name, 
                     String email, 
-                    LocalDate dob, 
-                    Integer age) {
+                    LocalDate dob
+                    ) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name, 
@@ -51,7 +47,6 @@ public class Student {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -87,7 +82,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
@@ -101,7 +96,6 @@ public class Student {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", dob='" + dob +
-                ", age='" + age +
                 '}';
     }
 }
